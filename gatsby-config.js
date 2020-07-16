338e99459933002d6563677d01be0fe586de72cc
Warren White <wwhite@samasource.org>
2020-07-16 18:09:43 -0400

    Initial commit from gatsby: (https://github.com/benrobertsonio/gatsby-starter-accessibility.git)

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    title: "Gatsby Accessibility Starter",
+    description:
+      "Kick off your next, great accessible Gatsby project with this starter. This starter ships with the main Gatsby configuration files you might need, as well as built-in jsx linting for accessibility errors, react-axe for catching accessibility errors in the DOM during production, and a storybook configured to test individual components for accessibility errors.",
+    author: "@gatsbyjs",
+  },
+  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-react-axe"],
+}
