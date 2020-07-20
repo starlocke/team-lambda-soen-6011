87931944e0a891c75359eb1efe739b8171588b29
Warren White <wwhite@samasource.org>
2020-07-19 23:54:09 -0400

    add a homepage link to the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,14 @@ import React from "react"
 
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
+import { HomePageLink } from '../components/homePageLink'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>PAGE NOT FOUND</h1>
+    <p>The page you tried to access could not be found</p>
+    <HomePageLink />
   </Layout>
 )
 
