d0a7a5f4707a154e124d58cc7a83cd5593ff2a39
Warren White <wwhite@samasource.org>
2020-08-12 16:10:42 -0400

    add content for intro

---
+++ src/components/introContainer/introContainer.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "./introContainer.css"
+
+const introContainer = () => (
+    <section id = "intro__container">
+        <h2>What is SESTOPIA?</h2>
+        <div>
+            <em>SESTOPIA</em> is an information-intensive web application
+            built upon the information extracted 
+            from the 15 different knowledge areas of SWEBOK which 
+            stands for a <em>“Guide to the Software Engineering Body of Knowledge”</em>. 
+            Every team member has uniquely chosen a knowledge area and 
+            has written an article that explains at length certain important aspects of
+            a skill that they are passionate about within that knowledge area.
+        </div>
+    </section>
+)
+
+export default introContainer;
